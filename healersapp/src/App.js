da82a1a58069918ea5f24794ac3c8e6f3fdb9cb3
youmna salloum <53618405+YoumnaSalloum@users.noreply.github.com>
2020-07-22 16:13:20 +0300

    Revert "(feat)notification on email of patient in backend"

--- healersapp/src/App.js
+++ healersapp/src/App.js
@@ -1,35 +1,26 @@
-//make sure that all components have the same name of routs
 import React from 'react';
-//import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import NavBar from './components/navbar.js';
+import logo from './logo.svg';
+import './App.css';
 
-import HospitalBill from "./components/hospitalbill.js";
-import BillForm from "./components/hosform.js";
-import IntersetForm from "./components/intresetform.js";
-import Login from "./components/login.js";
-import Signup from "./components/signup.js";
-
-
-function App(){
+function App() {
   return (
-    
-     <Router>
-     <br/> 
-     <div>
-     
-     
-     <Route path="/hospitalbill" exact component= {HospitalBill} />
-     <Route path="/hosform" exact component= {BillForm} />
-     <Route path="/IntersetForm" exact component= {IntersetForm} />
-     <Route path="/Login" exact component= {Login} />
-     <Route path="/" exact component= {NavBar} />
-     <Route path="/Signup" exact component= {Signup} />
-    
-     </div>
-     </Router>
-     
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
